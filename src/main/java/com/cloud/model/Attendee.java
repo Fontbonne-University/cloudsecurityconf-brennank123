2dd63acd42341eb6180de961331f4f836136d02b
brennank <brennank@fontbonne.edu>
2018-04-19 18:18:10 -0500

    Added name, phone attributes to Attendee class and added get and set methods for new attributes

--- src/main/java/com/cloud/model/Attendee.java
+++ src/main/java/com/cloud/model/Attendee.java
@@ -1,14 +1,16 @@
 package com.cloud.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+        import javax.persistence.Entity;
+        import javax.persistence.Id;
+        import javax.persistence.Table;
 
 @Entity
 @Table(name="attendee", schema = "public")
 public class Attendee {
     @Id
     String email;
+    String name;
+    String phone;
 
     public String getEmail() {
         return email;
@@ -16,5 +18,20 @@ public class Attendee {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 }
